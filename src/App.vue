d41743ce61ed76c3eeb173870bf6f8301b4ea860
Your Name <you@example.com>
2022-04-05 17:45:04 +0200

    fele kész

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <ul class="navbar-nav">
+        <li class="nav-item m-2">
+          <a href="" class="nav-link">asd</a>
+        </li>
+        <li class="nav-item m-2">
+          <a href="" class="nav-link">asd</a>
+        </li>
+      </ul>
+  </nav>
+  
+  <div class="row m-1" id="tablazat">
+    <div v-for="vizsgaidopont in vizsgaidopontok" :key="vizsgaidopont.id" class="col-xl-4 col-md-6 col-12">
+      <div class="m-2 card p-3">
+        <h2>{{vizsgaidopont.targy}}</h2>
+        <h4>{{vizsgaidopont.tipus}}</h4>
+        <p>{{vizsgaidopont.kezdes}}</p>
+      </div>
+    </div>
+  </div>
+  <div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      vizsgaidopontok: [],
+    }
+  },
+  methods: {
+    async feltoltes() {
+      await axios
+        .get('http://127.0.0.1:8000/api/vizsgaidopontok')
+        .then(response => this.vizsgaidopontok = response.data)
+    }
+  },
+  mounted() {
+    this.feltoltes()
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-createApp(App).mount('#app')
+createApp(App).use(VueAxios, axios).mount('#app')
